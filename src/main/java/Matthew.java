9218ce280ee219452d7a97bba77303eb87c8ecff
timthedev07 <timpersonal07@gmail.com>
2026-08-18 09:48:29 +0800

    Change entry message

--- src/main/java/Matthew.java
+++ src/main/java/Matthew.java
@@ -1,10 +1,18 @@
 public class Matthew {
     public static void main(String[] args) {
-        String banner = " __  __       _   _   _                   \n"
+
+        String line = "____________________________________________________________\n";
+        String banner = line
+                      + " __  __       _   _   _                   \n"
                       + "|  \\/  | __ _| |_| |_| |__   _____      __\n"
                       + "| |\\/| |/ _` | __| __| '_ \\ / _ \\ \\ /\\ / /\n"
                       + "| |  | | (_| | |_| |_| | | |  __/\\ V  V / \n"
-                      + "|_|  |_|\\__,_|\\__|\\__|_| |_|\\___| \\_/\\_/  \n";
+                      + "|_|  |_|\\__,_|\\__|\\__|_| |_|\\___| \\_/\\_/  \n"
+                      + "Hello! I'm Matthew.\n"
+                      + "What can I do for you?\n"
+                      + line
+                      + "Bye."
+                      + line;
 
         System.out.println(banner);
     }
