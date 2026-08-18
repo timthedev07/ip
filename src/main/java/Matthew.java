49d61bfe263ec75f3336a87ea0836b3ff3322b28
timthedev07 <timpersonal07@gmail.com>
2026-08-18 10:18:39 +0800

    Complete echo task

--- src/main/java/Matthew.java
+++ src/main/java/Matthew.java
@@ -1,7 +1,7 @@
 public class Matthew {
+    private static final String line = "____________________________________________________________\n";
     public static void main(String[] args) {
 
-        String line = "____________________________________________________________\n";
         String banner = line
                       + " __  __       _   _   _                   \n"
                       + "|  \\/  | __ _| |_| |_| |__   _____      __\n"
@@ -10,10 +10,22 @@ public class Matthew {
                       + "|_|  |_|\\__,_|\\__|\\__|_| |_|\\___| \\_/\\_/  \n"
                       + "Hello! I'm Matthew.\n"
                       + "What can I do for you?\n"
-                      + line
-                      + "Bye."
                       + line;
 
         System.out.println(banner);
+
+
+        String response = "";
+
+        while (!response.equals("bye")) {
+            response = System.console().readLine();
+            System.out.println(wrap(response));
+        }
+
+        System.out.println(wrap("Goodbye! Have a nice day!"));
     }
+    public static String wrap(String t) {
+        return line + t + "\n" + line;
+    }
+
 }
