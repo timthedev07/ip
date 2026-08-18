ff332549a233c3efc855842aaab9cefe4304debe
timthedev07 <timpersonal07@gmail.com>
2026-08-18 11:09:09 +0800

    Complete level 5

--- src/main/java/Matthew.java
+++ src/main/java/Matthew.java
@@ -22,87 +22,207 @@ public class Matthew {
         while (true) {
             String response = System.console().readLine();
 
-            if (response.equals("bye")) {
-                break;
+            try {
+                if (response.equals("bye")) {
+                    break;
 
-            } else if (response.equals("list")) {
-                System.out.print(line);
-                System.out.println("Here are the tasks in your list:");
+                } else if (response.equals("list")) {
+                    System.out.print(line);
+                    System.out.println("Here are the tasks in your list:");
 
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + "." + tasks[i]);
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + "." + tasks[i]);
+                    }
+
+                    System.out.print(line);
+
+                } else if (response.startsWith("mark")) {
+                    if (!response.startsWith("mark ")) {
+                        throw new MatthewException(
+                                "Please tell me which task to mark, e.g. mark 2.");
+                    }
+
+                    int taskNumber = getTaskNumber(
+                            response.substring(5), taskCount);
+
+                    Task task = tasks[taskNumber - 1];
+                    task.markAsDone();
+
+                    System.out.print(line);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + task);
+                    System.out.print(line);
+
+                } else if (response.startsWith("unmark")) {
+                    if (!response.startsWith("unmark ")) {
+                        throw new MatthewException(
+                                "Please tell me which task to unmark, e.g. unmark 2.");
+                    }
+
+                    int taskNumber = getTaskNumber(
+                            response.substring(7), taskCount);
+
+                    Task task = tasks[taskNumber - 1];
+                    task.markAsNotDone();
+
+                    System.out.print(line);
+                    System.out.println(
+                            "OK, I've marked this task as not done yet:");
+                    System.out.println("  " + task);
+                    System.out.print(line);
+
+                } else if (response.startsWith("todo")) {
+                    if (!response.startsWith("todo ")) {
+                        throw new MatthewException(
+                                "A todo needs a description.");
+                    }
+
+                    String description = response.substring(5).trim();
+
+                    if (description.isEmpty()) {
+                        throw new MatthewException(
+                                "A todo needs a description.");
+                    }
+
+                    if (taskCount >= tasks.length) {
+                        throw new MatthewException(
+                                "Your task list is full.");
+                    }
+
+                    Task task = new Todo(description);
+                    tasks[taskCount++] = task;
+
+                    printAddedTask(task, taskCount);
+
+                } else if (response.startsWith("deadline")) {
+                    if (!response.startsWith("deadline ")) {
+                        throw new MatthewException(
+                                "A deadline needs a description and /by time.");
+                    }
+
+                    String input = response.substring(9).trim();
+                    int byIndex = input.indexOf(" /by ");
+
+                    if (byIndex == -1) {
+                        throw new MatthewException(
+                                "A deadline must include '/by'. "
+                                        + "For example: deadline return book /by Sunday");
+                    }
+
+                    String description =
+                            input.substring(0, byIndex).trim();
+                    String by =
+                            input.substring(byIndex + 5).trim();
+
+                    if (description.isEmpty()) {
+                        throw new MatthewException(
+                                "A deadline needs a description.");
+                    }
+
+                    if (by.isEmpty()) {
+                        throw new MatthewException(
+                                "A deadline needs a time after '/by'.");
+                    }
+
+                    if (taskCount >= tasks.length) {
+                        throw new MatthewException(
+                                "Your task list is full.");
+                    }
+
+                    Task task = new Deadline(description, by);
+                    tasks[taskCount++] = task;
+
+                    printAddedTask(task, taskCount);
+
+                } else if (response.startsWith("event")) {
+                    if (!response.startsWith("event ")) {
+                        throw new MatthewException(
+                                "An event needs a description, /from time and /to time.");
+                    }
+
+                    String input = response.substring(6).trim();
+
+                    int fromIndex = input.indexOf(" /from ");
+                    int toIndex = input.indexOf(" /to ");
+
+                    if (fromIndex == -1 || toIndex == -1
+                            || toIndex < fromIndex) {
+                        throw new MatthewException(
+                                "An event must use '/from' and '/to'. "
+                                        + "For example: event meeting /from Mon 2pm /to 4pm");
+                    }
+
+                    String description =
+                            input.substring(0, fromIndex).trim();
+                    String from =
+                            input.substring(fromIndex + 7, toIndex).trim();
+                    String to =
+                            input.substring(toIndex + 5).trim();
+
+                    if (description.isEmpty()) {
+                        throw new MatthewException(
+                                "An event needs a description.");
+                    }
+
+                    if (from.isEmpty()) {
+                        throw new MatthewException(
+                                "An event needs a start time after '/from'.");
+                    }
+
+                    if (to.isEmpty()) {
+                        throw new MatthewException(
+                                "An event needs an end time after '/to'.");
+                    }
+
+                    if (taskCount >= tasks.length) {
+                        throw new MatthewException(
+                                "Your task list is full.");
+                    }
+
+                    Task task = new Event(description, from, to);
+                    tasks[taskCount++] = task;
+
+                    printAddedTask(task, taskCount);
+
+                } else {
+                    throw new MatthewException(
+                            "I don't recognise that command.");
                 }
 
-                System.out.print(line);
-
-            } else if (response.startsWith("mark ")) {
-                int taskNumber = Integer.parseInt(response.substring(5));
-                Task task = tasks[taskNumber - 1];
-
-                task.markAsDone();
-
-                System.out.print(line);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + task);
-                System.out.print(line);
-
-            } else if (response.startsWith("unmark ")) {
-                int taskNumber = Integer.parseInt(response.substring(7));
-                Task task = tasks[taskNumber - 1];
-
-                task.markAsNotDone();
-
-                System.out.print(line);
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + task);
-                System.out.print(line);
-
-            } else if (response.startsWith("todo ")) {
-                String description = response.substring(5);
-
-                Task task = new Todo(description);
-                tasks[taskCount++] = task;
-
-                printAddedTask(task, taskCount);
-
-            } else if (response.startsWith("deadline ")) {
-                String input = response.substring(9);
-
-                int byIndex = input.indexOf(" /by ");
-
-                String description = input.substring(0, byIndex);
-                String by = input.substring(byIndex + 5);
-
-                Task task = new Deadline(description, by);
-                tasks[taskCount++] = task;
-
-                printAddedTask(task, taskCount);
-
-            } else if (response.startsWith("event ")) {
-                String input = response.substring(6);
-
-                int fromIndex = input.indexOf(" /from ");
-                int toIndex = input.indexOf(" /to ");
-
-                String description = input.substring(0, fromIndex);
-                String from = input.substring(fromIndex + 7, toIndex);
-                String to = input.substring(toIndex + 5);
-
-                Task task = new Event(description, from, to);
-                tasks[taskCount++] = task;
-
-                printAddedTask(task, taskCount);
+            } catch (MatthewException e) {
+                System.out.println(wrap("OOPS!!! " + e.getMessage()));
             }
         }
 
         System.out.println(wrap("Goodbye! Have a nice day!"));
     }
 
+    public static int getTaskNumber(String input, int taskCount)
+            throws MatthewException {
+
+        int taskNumber;
+
+        try {
+            taskNumber = Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new MatthewException(
+                    "The task number must be a number.");
+        }
+
+        if (taskNumber < 1 || taskNumber > taskCount) {
+            throw new MatthewException(
+                    "There is no task numbered " + taskNumber + ".");
+        }
+
+        return taskNumber;
+    }
+
     public static void printAddedTask(Task task, int taskCount) {
         System.out.print(line);
         System.out.println("Got it. I've added this task:");
         System.out.println("  " + task);
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
+        System.out.println(
+                "Now you have " + taskCount + " tasks in the list.");
         System.out.print(line);
     }
 
---
+++ src/main/java/MatthewException.java
@@ -0,0 +1,6 @@
+public class MatthewException extends Exception {
+
+    public MatthewException(String message) {
+        super(message);
+    }
+}
